124c56c5de93c99c42cc1cd737c95effddb740d9
Pamela Badermann <badermann.pamela@gmail.com>
2021-07-18 12:56:36 -0300

    sixth commit

---
+++ dist/core/data/database/migrations/1626621620571-CreateTableUsers.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateTableUsers1625759873287 = void 0;
+const typeorm_1 = require("typeorm");
+class CreateTableUsers1625759873287 {
+    async up(queryRunner) {
+        await queryRunner.createTable(new typeorm_1.Table({
+            name: 'users',
+            columns: [
+                {
+                    name: 'uid',
+                    type: 'uuid',
+                    isPrimary: true,
+                    isNullable: false
+                },
+                {
+                    name: 'username',
+                    type: 'varchar',
+                    length: '25',
+                    isNullable: false
+                },
+                {
+                    name: 'password',
+                    type: 'varchar',
+                    length: '25',
+                    isNullable: false
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    isNullable: false
+                },
+                {
+                    name: 'updated_at',
+                    type: 'timestamp',
+                    isNullable: false
+                }
+            ]
+        }));
+    }
+    async down(queryRunner) {
+        await queryRunner.dropTable('users', true, true, true);
+    }
+}
+exports.CreateTableUsers1625759873287 = CreateTableUsers1625759873287;
